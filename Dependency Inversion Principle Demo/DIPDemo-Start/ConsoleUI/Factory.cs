644818c549ea7e0b2b990210569639a7102ffaa3
7GitHub7 <amrozp@gmail.com>
2021-05-09 19:46:25 +0200

    Created Factory class, extracted interfaces for classes

---
+++ Dependency Inversion Principle Demo/DIPDemo-Start/ConsoleUI/Factory.cs
@@ -0,0 +1,33 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Factory
+    {
+        public static IPerson CreatePerson()
+        {
+            return new Person();
+
+        }
+
+        public static IChore CreateChore()
+        {
+            return new Chore();
+        }
+
+        public static ILogger CreateLogger()
+        {
+            return new Logger();
+        }
+
+        public static IMessageSender CreateMessageSender()
+        {
+            return new Emailer();
+        }
+    }
+}
---
+++ Dependency Inversion Principle Demo/DIPDemo-Start/ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPerson owner = Factory.CreatePerson();
+            owner.FirstName = "Test";
+            owner.LastName = "test";
+            owner.EmailAddress = "test@test.com";
+            owner.PhoneNumber = "555-1212";
+
+
+            IChore chore = Factory.CreateChore();
+
+            chore.ChoreName = "Take out the trash";
+            chore.Owner = owner;
+
+
+            chore.PerformedWork(3);
+            chore.PerformedWork(1.5);
+            chore.CompleteChore();
+
+            Console.ReadLine();
+        }
+    }
+}
