b5d6607390d5c624829420f1656cdb31518ef0e7
Piotrek <amrozp@gmail.com>
2021-05-14 09:56:45 +0200

    init lecture with dapper

---
+++ DDD/Gifter.Persistance.Dapper.SQLite/Interfaces/ByWhatId.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gifter.Persistance.Dapper.SQLite.Interfaces
+{
+    public enum ByWhatId
+    {
+        UniqueId = 0,
+        CreatedId = 1,
+    }
+}
---
+++ DDD/Gifter.Persistance.Dapper.SQLite/Interfaces/Doer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gifter.Persistance.Dapper.SQLite.Interfaces
+{
+    class Doer
+    {
+        public abstract class BeforeDoer : IBeforeDoer
+        {
+            protected IGeekLemonDBContext _geekLemonContext;
+
+            public void ChangeDBContext(IGeekLemonDBContext context)
+            {
+                _geekLemonContext = context;
+            }
+
+
+        }
+
+        public class DapperDbConnectionFactory : IDbConnectionFactory
+        {
+            private readonly IDictionary<string, string> _connectionDict;
+
+            public DapperDbConnectionFactory(IDictionary<string, string> connectionDict)
+            {
+                _connectionDict = connectionDict;
+            }
+
+            public DbConnection CreateConnection(string nameOrConnectionString)
+            {
+                string connectionString = null;
+                if (_connectionDict.TryGetValue(nameOrConnectionString, out connectionString))
+                {
+                    return new SqlConnection(connectionString);
+                }
+
+                throw new ArgumentNullException();
+            }
+
+        }
+    }
+}
