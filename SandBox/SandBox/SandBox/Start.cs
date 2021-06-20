d63e6397fd3868a3ed47d1b127318daa2d3829bf
Piotrek <amrozp@gmail.com>
2021-06-20 15:52:02 +0200

    added sandbox for testing

---
+++ SandBox/SandBox/SandBox/Start.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SandBox
+{
+    interface IVehicle
+    {
+        void getName();
+        void getType();
+
+    }
+    public class Car : IVehicle
+    {
+
+        public void getName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void getType()
+        {
+            throw new NotImplementedException();
+        }
+        void printRoad() {
+            Console.WriteLine("ss");
+        }
+
+    }
+
+    public class Start
+    {
+
+        // Main Method
+        static public void Main(String[] args)
+        {
+            List<IVehicle> CarList = new List<IVehicle>();
+            CarList.Add(new Car());
+
+
+            Console.WriteLine("Main Method");
+        }
+    }
+}
